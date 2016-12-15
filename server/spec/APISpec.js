6f962098a0ae580d3394a3f7308fb9e7ae916fad
Riva GiovanniMaria (ACC0085672) <giovannimaria.riva@sci-b106pc020x.unitn.it>
2016-12-15 10:40:58 +0100

    implement deleteStudent Test

--- server/spec/APISpec.js
+++ server/spec/APISpec.js
@@ -78,3 +78,93 @@ describe("Test /searchStudent", function() {
 
 
 });
+
+// Test for /deleteStudent
+describe("Test /deleteStudent", function() {
+	//set the data
+	var data = {ID: '1', SSN: 'AB45'};
+	
+	//legal request
+	it("to returns status code 200", function(done) {
+	  client.post(base_url + "deleteStudent/", data, function(err, res, body) {
+		expect(body).toEqual(
+			{
+				ID: "1",
+				SSN: "AB45", 
+				name: "Mattia",
+				address: "via Roma",
+				mark: "5"
+			}
+		);
+
+		done();
+	  });
+	});
+	
+	var data = {ID: '1'};
+
+	//legal request
+	it("to returns status code 200", function(done) {
+	  client.post(base_url + "deleteStudent/", data, function(err, res, body) {
+		expect(body).toEqual(
+			{
+				ID: "1",
+				SSN: "AB45", 
+				name: "Mattia",
+				address: "via Roma",
+				mark: "5"
+			}
+		);
+
+		done();
+	  });
+	});
+	
+	var data = {SSN: 'AB45'};
+
+	//legal request
+	it("to returns status code 200", function(done) {
+	  client.post(base_url + "deleteStudent/", data, function(err, res, body) {
+		expect(body).toEqual(
+			{
+				ID: "1",
+				SSN: "AB45", 
+				name: "Mattia",
+				address: "via Roma",
+				mark: "5"
+			}
+		);
+
+		done();
+	  });
+	});
+	
+	//student non existing
+	data1 = {ID: "10" };
+	it("to returns status code 406", function(done) {
+	  client.post(base_url + "deleteStudent/", data1, function(err, res, body) {
+		expect(res.statusCode).toBe(404);
+		done();
+	  });
+	});
+	
+	data1 = {SSN: "AD74" };
+	it("to returns status code 406", function(done) {
+	  client.post(base_url + "deleteStudent/", data1, function(err, res, body) {
+		expect(res.statusCode).toBe(404);
+		done();
+	  });
+	});
+	
+	//wrong parameter
+	data2 = {name: "1" };
+	it("to returns status code 406", function(done) {
+	  client.post(base_url + "deleteStudent/", data2, function(err, res, body) {
+		expect(res.statusCode).toBe(406);
+		expect(body).toBe(1);
+		done();
+	  });
+	});
+
+
+});
